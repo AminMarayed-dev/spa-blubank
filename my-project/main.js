a2fd5dbc42fefb64145c316120bf746f8065444d
AminMarayed <aminmarayed4@gmail.com>
2024-03-23 23:10:33 +0330

    import router , app and navigate in main.js

--- my-project/main.js
+++ my-project/main.js
@@ -1,6 +1,15 @@
-import './src/styles/index.css';
-import { App } from './src/app.js';
+import "./src/styles/index.css";
+import { App } from "./src/app.js";
+import { router, navigateTo } from "./src/js/router.js";
 
-document.querySelector('#app').append(App());
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    const app = App();
+    app.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.id === "link") {
+        navigateTo(e.target.href);
+      }
+    });
+    router();
+  });
+  window.addEventListener("popstate", router);
